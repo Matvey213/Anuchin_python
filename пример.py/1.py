d50358e6e887387b6e933e94492e3921886b6169
Matvey213 <ama030204@gmail.com>
2022-10-28 23:53:28 +0300

    sfd

---
+++ пример.py/1.py
@@ -0,0 +1,10 @@
+werb = int (input("число")) 
+list_1 = [1, 2, 3]
+def var(werb):
+    if werb % 10 == 0:
+        print(list_1)
+    else:
+        print("hallo word")
+    return werb
+print(var(werb))
+
